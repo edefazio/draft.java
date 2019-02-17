d8c8f4480a291bd918bfa1a2cb029ce634d015e4
Eric DeFazio <medefazio@yahoo.com>
2019-02-17 15:22:38 -0500

    Named method made more abstract in preparation for Diff

---
+++ src/main/java/draft/Named.java
@@ -0,0 +1,13 @@
+package draft;
+
+/**
+ * An entity that can be identified by a name
+ * This is also used for Standardizing names across components 
+ * (i.e. {@link draft.java._java.Part} )
+ * 
+ * @author Eric
+ */
+public interface Named {
+    
+    public String getName();    
+}
