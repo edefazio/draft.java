5dcd2f49d829d5c65184e391542f078fc2da1ea8
Eric DeFazio <medefazio@yahoo.com>
2019-02-17 15:21:50 -0500

    componentize method made more adbstract

---
+++ src/main/java/draft/Composite.java
@@ -0,0 +1,31 @@
+package draft;
+
+import java.util.Map;
+
+/**
+ * An entity made of constituent "parts" that can be broken down into
+ * individual components.
+ * 
+ * NOTE: 
+ * <PRE>
+ * for example: 
+ * {@link draft.java._class} which can "componentized" into
+ * {@link draft.java._field}s, {@link draft.java._method}, ... etc. 
+ * </PRE>
+ * 
+ * @see draft.java._class#componentize() for an example 
+ * Assembler
+ * 
+ * Disassembler
+ * 
+ * @author Eric
+ */
+public interface Composite {
+    
+   /**
+    * build a Map consisting of the Named components that make up the Composite
+    * @return a map containing the named components of the composite
+    */ 
+   public Map<String,Object> componentize();
+   
+}
