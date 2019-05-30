b2e22633130fa6068e02427ae2c55e7b445cb898
Eric DeFazio <medefazio@yahoo.com>
2019-05-29 23:11:10 -0400

    added Path to the _in._source to help set the storage on the JavaParser's AST Storage (to link back to the file that originated the AST)

--- src/main/java/draft/java/io/_in.java
+++ src/main/java/draft/java/io/_in.java
@@ -1,6 +1,7 @@
 package draft.java.io;
 
 import java.io.InputStream;
+import java.nio.file.Path;
 
 /**
  * Input sources to be read from (i.e. .java source code) and resolvers
@@ -30,6 +31,8 @@ public interface _in {
      * @return source input walk
      */
     InputStream getInputStream();
+    
+    Path getPath();
 
     /**
      * Implementation of an _in (input source)
@@ -46,18 +49,38 @@ public interface _in {
             return new _source( sourceId, description, is );
         }
 
+        /**
+         * @param sourceId
+         * @param description
+         * @param is
+         * @return
+         */
+        public static _source of(Path path, String sourceId, String description, InputStream is ) {
+            _source _s = new _source( sourceId, description, is );
+            _s.path = path;
+            return _s;
+        }
+        
+        /** the (optional) path to where the file loaded it*/
+        public Path path;
+        
         private final String description;
 
         private final String sourceId;
 
         private final InputStream is;
 
-        public _source( String sourceId, String description, InputStream is ) {
+        public _source( Path path, String sourceId, String description, InputStream is ) {
             this.description = description;
             this.sourceId = sourceId;
             this.is = is;
         }
+        
+        public _source( String sourceId, String description, InputStream is ) {
+            this( null, sourceId, description, is);
+        }
 
+        
         @Override
         public String describe() {
             return description;
@@ -73,6 +96,11 @@ public interface _in {
             return this.is;
         }
 
+        @Override
+        public Path getPath(){
+            return this.path;
+        }
+        
         @Override
         public String toString(){
             return getSourceId()+" "+ describe();
