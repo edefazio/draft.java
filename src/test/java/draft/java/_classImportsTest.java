5f7383ecb810b5a5172a58c563a5ba9362ee0f33
Eric DeFazio <medefazio@yahoo.com>
2019-06-29 17:14:15 -0400

    added a test for verifying that we fixed an issue in _code where an array of Classes are sent in to addImports() and the first one NOT added breaks the entire thing (and later Classes are not imported)

---
+++ src/test/java/draft/java/_classImportsTest.java
@@ -0,0 +1,22 @@
+
+package draft.java;
+
+import java.util.UUID;
+import junit.framework.TestCase;
+
+/**
+ *
+ * @author Eric
+ */
+public class _classImportsTest extends TestCase {
+    
+    //Fix bug where we break when we encounter the first import that is already 
+    //imported
+    public void test_class(){
+        _class _c = _class.of("aaaa.bbbb.N");
+        _c.imports(String.class, UUID.class);
+        System.out.println( _c );
+        assertTrue( _c.hasImport(UUID.class));
+    }
+    
+}
